6b21af90802989f4e18d4336a3caade218b3ecb4
Mohammad Soqar <mnsoqar1@gmail.com>
2025-01-17 13:13:06 +0300

    Update error_message_widget.dart

--- lib/ui/widgets/error_message_widget.dart
+++ lib/ui/widgets/error_message_widget.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class ErrorMessageWidget extends StatelessWidget {
   final String errorMessage;
 
-  const ErrorMessageWidget(this.errorMessage);
+  const ErrorMessageWidget(this.errorMessage, {super.key});
 
   @override
   Widget build(BuildContext context) {
