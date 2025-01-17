2a843f4933be20527dc265053ce448e3dc8d3be1
Mohammad Soqar <mnsoqar1@gmail.com>
2025-01-17 13:13:22 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:plantai/ui/views/plant_list_view.dart';
+import 'package:plantai/viewmodels/plant_list_viewmodel.dart';
+// ignore: depend_on_referenced_packages
+import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'ui/views/plant_view.dart';
 import 'viewmodels/plant_view_model.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => PlantViewModel()),
+        ChangeNotifierProvider(create: (_) => PlantListViewmodel()),
       ],
       child: MyApp(),
     ),
@@ -15,6 +22,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
