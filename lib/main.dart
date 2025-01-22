34c3799a2a8f4377ab636017d84f638d2561ad45
Mohammad Soqar <mnsoqar1@gmail.com>
2025-01-22 12:28:19 +0300

    Updating Providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:plantai/data/repositories/plant_repository.dart';
 import 'package:plantai/ui/views/plant_list_view.dart';
+import 'package:plantai/viewmodels/add_edit_plant_viewmodel.dart';
 import 'package:plantai/viewmodels/plant_list_viewmodel.dart';
 // ignore: depend_on_referenced_packages
 import 'package:firebase_core/firebase_core.dart';
@@ -10,11 +12,17 @@ import 'viewmodels/plant_view_model.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+ // Instantiate the repository
+  final plantRepository = PlantRepository();
+
+
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => PlantViewModel()),
         ChangeNotifierProvider(create: (_) => PlantListViewmodel()),
+        ChangeNotifierProvider(create: (_) => AddEditPlantViewModel(plantRepository)),
       ],
       child: MyApp(),
     ),
