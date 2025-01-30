a60e75516ad71c1453fb6dc99aad7262e5eb5ead
Mohammad Soqar <mnsoqar1@gmail.com>
2025-01-31 00:53:22 +0300

    change Data folder structure

--- lib/data/services/Firebase/firebase_auth_service.dart
+++ lib/data/services/Firebase/firebase_auth_service.dart
@@ -1,21 +1,14 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 // ignore: library_prefixes
 import 'package:plantai/data/models/user.dart' as userModel;
 
-
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  Future<void> registerUser(
-      String fullName,
-      String phoneNumber,
-      String email,
-      String password,
-      
-      String role) async {
+  Future<void> registerUser(String fullName, String phoneNumber, String email,
+      String password, String role) async {
     try {
       UserCredential userCredential =
           await _auth.createUserWithEmailAndPassword(
@@ -34,13 +27,11 @@ class AuthMethods {
         verified: false,
         createdAt: DateTime.now(),
         lastUpdatedAt: DateTime.now(),
-
       );
       // Save user data to the "users" collection
       await _firestore.collection('users').doc(uid).set(user.toJson());
 
       // Save patient data to the "patients" collection (if the user is a patient)
-     
     } catch (e) {
       print("Error registering user: $e");
       rethrow;
