5f4c6b6a8e078820f75226298343784c671b1f71
Mohammad Soqar <mnsoqar1@gmail.com>
2025-01-31 22:26:08 +0300

    Update home_page.dart

--- lib/ui/views/home_page.dart
+++ lib/ui/views/home_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:plantai/ui/views/plant_view.dart';
+import 'package:plantai/ui/widgets/plant_card_widget.dart';
 import 'package:plantai/viewmodels/plant_view_model.dart';
 import 'package:provider/provider.dart';
 
@@ -24,7 +25,7 @@ class HomePage extends StatelessWidget {
             backgroundColor: Colors.transparent,
             leading: const CircleAvatar(
               backgroundImage: NetworkImage(
-                  'https://www.google.com/url?sa=i&url=https%3A%2F%2Fes.pinterest.com%2Fpin%2F606156431091159176%2F&psig=AOvVaw0W5wrCyRzNzv745_fqAMp5&ust=1737827702209000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJigw-r2josDFQAAAAAdAAAAABAI'),
+                  'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'),
             ),
             title: const Text(
               'Hi, Mark',
@@ -39,12 +40,12 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
+      body: const SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Row(
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text('My Plants',
@@ -53,33 +54,41 @@ class HomePage extends StatelessWidget {
                 Text('View all', style: TextStyle(color: Colors.white)),
               ],
             ),
-            const SizedBox(height: 10),
+            SizedBox(height: 10),
             // My Plants Cards
-            SizedBox(
-              height: 120,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: const [
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
                   PlantCard(
-                      plantName: 'Snake Plant',
-                      humidity: '125%',
-                      sunlight: 'Sunny',
-                      temperature: '90°F'),
+                    plantName: 'Snake Plant',
+                    humidity: '125%',
+                    sunlight: 'Sunny',
+                    temperature: '90°F',
+                  ),
+                   PlantCard(
+                    plantName: 'Snake Plant',
+                    humidity: '125%',
+                    sunlight: 'Sunny',
+                    temperature: '90°F',
+                  ),
                   PlantCard(
-                      plantName: 'ZZ Plant',
-                      humidity: '125%',
-                      sunlight: 'Sunny',
-                      temperature: '92°F'),
+                    plantName: 'ZZ Plant',
+                    humidity: '125%',
+                    sunlight: 'Sunny',
+                    temperature: '92°F',
+                  ),
                 ],
               ),
             ),
-            const SizedBox(height: 20),
+
+            SizedBox(height: 20),
             // Related Plants Section
-            const Text('Related Plants',
+            Text('Related Plants',
                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            const SizedBox(height: 10),
+            SizedBox(height: 10),
             // Related Plants Cards
-            const ListTile(
+            ListTile(
               title: Text('Alberiya Garden Plant'),
               subtitle: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -133,9 +142,11 @@ class HomePage extends StatelessWidget {
                 if (image != null) {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => PlantView(plantPicture: image!,)),
+                    MaterialPageRoute(
+                        builder: (context) => PlantView(
+                              plantPicture: image!,
+                            )),
                   );
-                  
                 }
               }
             },
@@ -153,43 +164,6 @@ class HomePage extends StatelessWidget {
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-    );
-  }
-}
-
-class PlantCard extends StatelessWidget {
-  final String plantName;
-  final String humidity;
-  final String sunlight;
-  final String temperature;
-
-  const PlantCard({
-    super.key,
-    required this.plantName,
-    required this.humidity,
-    required this.sunlight,
-    required this.temperature,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(right: 16.0),
-      width: 150,
-      decoration: BoxDecoration(
-        color: Colors.green,
-        borderRadius: BorderRadius.circular(16.0),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const SizedBox(height: 10),
-          Text(plantName, style: const TextStyle(fontWeight: FontWeight.bold)),
-          const SizedBox(height: 5),
-          Text('$humidity • $sunlight • $temperature',
-              style: const TextStyle(fontSize: 12, color: Colors.grey)),
-        ],
-      ),
     );
   }
 }
