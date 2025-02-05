53dd4f77bcb10b02d2635adf9c5b6e55fed98c6e
Mohammad Soqar <mnsoqar1@gmail.com>
2025-02-05 21:03:26 +0300

    Update home_page.dart

--- lib/ui/views/home_page.dart
+++ lib/ui/views/home_page.dart
@@ -83,7 +83,7 @@ class HomePage extends StatelessWidget {
                       plantName: plant.plantName,
                       imageUrl: plant.imageUrl,
                       onTap: () {
-                        // Handle card tap (e.g., navigate to a details page)
+                       
                         showDialog(
                           context: context,
                           builder: (context) => AlertDialog(
