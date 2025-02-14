dcc471ef3bd63712572acfc348f52daae4732240
Mohammad Soqar <mnsoqar1@gmail.com>
2025-02-14 21:48:13 +0300

    Update plant_service.dart

--- lib/data/services/plant_service.dart
+++ lib/data/services/plant_service.dart
@@ -1,4 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+  import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:plantai/data/models/plant.dart';
 
 class PlantService {
